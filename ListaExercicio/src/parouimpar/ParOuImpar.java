1ff2a8d25e46d6f15d5c60b48e0e3423520ec685
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2017-02-22 21:43:35 -0300

    Parou impar

--- ListaExercicio/src/parouimpar/ParOuImpar.java
+++ ListaExercicio/src/parouimpar/ParOuImpar.java
@@ -1,10 +1,29 @@
 package parouimpar;
 
-public class ParOuImpar 
-{
-	public static void main(Strings [] args)
-	{
-		int i = 10;
-		
-	}
+import java.util.Scanner;
+/**
+ * 
+ * @author Hudson, Gabriel
+ *classe que checa se o numero inserido eh par ou impar
+ */
+
+public class ParOuImpar {
+
+	public static void main(String[] args) {
+		int i = 0;
+		Scanner leitor = new Scanner(System.in);
+		for (i = 0; i<10; i++){
+			System.out.println("Entrada numero "+ (i+1));
+			System.out.println("Digite um numero");
+			int numero = leitor.nextInt();
+			if (numero % 2 == 0) {
+				System.out.println("Numero Par");
+			}
+			else{
+				System.out.println("Numero Impar");
+			}
+			
+		}
+		leitor.close();
+}
 }
