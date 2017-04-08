e369933e9932c999e47525bd6e316d09795fdaa5
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2017-04-08 16:36:45 -0300

    Update PFisica.java

--- Seguradora/src/projeto/PFisica.java
+++ Seguradora/src/projeto/PFisica.java
@@ -1,5 +1,12 @@
 package projeto;
 
-public class PFisica {
+public class PFisica extends Cliente{
+	
 
+	private String cpf;
+	
+	private int valImovel;
+	private String zona;
+	private String tipo;
+	
 }
