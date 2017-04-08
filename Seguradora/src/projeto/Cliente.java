2df792ccd5c9ce81bd89a37e6b1bbbf08533be90
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2017-04-08 17:48:45 -0300

    Update Cliente.java

--- Seguradora/src/projeto/Cliente.java
+++ Seguradora/src/projeto/Cliente.java
@@ -3,23 +3,21 @@ package projeto;
 import java.util.Scanner;
 
 public class Cliente {
-	Scanner ler = new Scanner(System.in);
 	public String nome;
-	public int valImovel;
 	public int tipoPessoa;
-	public static void main(String[] args){
-		Cliente pessoa = new Cliente();
-		System.out.println("Pessoa fisica(1) ou Juridica(2) >>");
-		tipoPessoa = ler.nextInt();
-		
-		if (tipoPessoa == 1){
-			// pessoa fisica
-		}
-		if (tipoPessoa == 2){
-			// pessoa juridica
-		}
+	
+	public String getNome() {
+		return nome;
 	}
-
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getTipoPessoa() {
+		return tipoPessoa;
+	}
+	public void setTipoPessoa(int tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
 	
 }
 
