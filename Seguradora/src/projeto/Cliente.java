17637370535bfee12281eebade751fceb620d165
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2017-04-08 16:34:42 -0300

    Update and rename Pessoa.java to Cliente.java

---
+++ Seguradora/src/projeto/Cliente.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class Cliente {
+	public String nome;
+	public int valImovel;
+	public int TipoPessoa;
+	public static void main(String[] args){
+		Cliente pessoa = new Cliente();
+	}
+
+	
+}
+
