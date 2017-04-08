5962710cbaa35fc6eaf2da976376040d00012707
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2017-04-08 17:48:16 -0300

    Update PJuridica.java

--- Seguradora/src/projeto/PJuridica.java
+++ Seguradora/src/projeto/PJuridica.java
@@ -1,9 +1,9 @@
 package projeto;
-
+// dados presentes na pessoa juridica
 public class PJuridica extends Cliente{
 	private String cnpj;
 	private int funcionarios;
-	private int nVisitas;
+	private int nvisitas;
 	
 
 }
